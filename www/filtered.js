37fcaeeffadbade20eb020b06ffa05255303c4a5
GG2015 <inquiry.globalgamer2015@gmail.com>
2021-08-30 06:22:31 -0400

    Added filter script

---
+++ www/filtered.js
@@ -0,0 +1,8 @@
+function filtered(data) {
+    if(data.is_filtered == 0) {
+        return is_filtered = "False";
+    }
+    else if(data.is_filtered == 1) {
+        return is_filtered = "True";
+    }
+}
